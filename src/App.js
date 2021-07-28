caabae038e91fb56429723236696f26f3d9e2db6
Zachary Lasky <zachary.lasky@W-DH-ML-ZLASKY2.local>
2021-07-28 10:26:22 -0400

    add click check

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import * as appStyle from './AppStyle';
 import * as lib from './lib';
 import * as assets from './assets';
 import { HomePage, DescriptivePage } from './components';
 
 export default function App() {
+  const [chevronClickCount, setChevronClickCount] = useState(0);
+
   const topRef = useRef();
   const missionRef = useRef();
   const purposeRef = useRef();
   const promiseRef = useRef();
+
+  useEffect(() => {
+    if (chevronClickCount === 1) {
+      missionRef.current.scrollIntoView({ behavior: 'smooth' });
+      return;
+    }
+
+    if (chevronClickCount === 2) {
+      purposeRef.current.scrollIntoView({ behavior: 'smooth' });
+      return;
+    }
+
+    if (chevronClickCount === 3) {
+      promiseRef.current.scrollIntoView({ behavior: 'smooth' });
+      return;
+    }
+
+    topRef.current.scrollIntoView({ behavior: 'smooth' });
+    setChevronClickCount(0);
+  }, [chevronClickCount]);
 
   return (
     <appStyle.Container className="app-container">
@@ -23,9 +45,8 @@ export default function App() {
       </appStyle.ComponentWrapper>
       <appStyle.ComponentWrapper className="app-promise-wrapper" ref={promiseRef}>
         <DescriptivePage content={lib.promiseContent} />
-        <appStyle.ChevronWrapper
-          onClick={() => missionRef.current.scrollIntoView({ behavior: 'smooth' })}>
-          {assets.chevronDown}
+        <appStyle.ChevronWrapper onClick={() => setChevronClickCount(chevronClickCount + 1)}>
+          {chevronClickCount === 3 ? assets.chevronUp : assets.chevronDown}
         </appStyle.ChevronWrapper>
         <appStyle.SectionFooter>
           <appStyle.LogoWrapper>{assets.logo}</appStyle.LogoWrapper>
